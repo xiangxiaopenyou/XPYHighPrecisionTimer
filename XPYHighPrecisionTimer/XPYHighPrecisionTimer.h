005a1f1238462088f49c492b8d0aac8d49b72b67
xiangxiaopenyou <linpingxiang@faceunity.com>
2022-09-27 15:50:29 +0800

    Initial.

---
+++ XPYHighPrecisionTimer/XPYHighPrecisionTimer.h
@@ -0,0 +1,18 @@
+//
+//  XPYHighPrecisionTimer.h
+//  XPYHighPrecisionTimer
+//
+//  Created by 项林平 on 2022/9/27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XPYHighPrecisionTimer.
+FOUNDATION_EXPORT double XPYHighPrecisionTimerVersionNumber;
+
+//! Project version string for XPYHighPrecisionTimer.
+FOUNDATION_EXPORT const unsigned char XPYHighPrecisionTimerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XPYHighPrecisionTimer/PublicHeader.h>
+
+
